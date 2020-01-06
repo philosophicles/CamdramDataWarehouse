495ad382239496a471540c0c294a2cee1e1884e9
Stuart Cuthbertson <5232616+philosophicles@users.noreply.github.com>
2020-01-05 16:34:50 -0800

    Fix typo

--- generate_output.sh
+++ generate_output.sh
@@ -5,7 +5,7 @@ PASS=
 
 mysql -u $USER -p$PASS -e "call output_dim_date();" camdram_dw > output/dim_date.tsv
 mysql -u $USER -p$PASS -e "call output_dim_society();" camdram_dw > output/dim_society.tsv
-mysql -u $USER -p$PASS -e "call output_dim_society_combo();" camdram_dw > output/output_dim_society_combo.tsv
+mysql -u $USER -p$PASS -e "call output_dim_society_combo();" camdram_dw > output/dim_society_combo.tsv
 mysql -u $USER -p$PASS -e "call output_dim_story();" camdram_dw > output/dim_story.tsv
 mysql -u $USER -p$PASS -e "call output_dim_time();" camdram_dw > output/dim_time.tsv
 mysql -u $USER -p$PASS -e "call output_dim_venue();" camdram_dw > output/dim_venue.tsv
